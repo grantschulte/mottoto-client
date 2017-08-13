e37165a87684a59abcca30ba9ecd1c801c0bc8c2
Grant Schulte <schulte.grant@gmail.com>
2017-08-13 18:56:45 -0400

    Welcome page buttons

--- src/Views/Welcome.elm
+++ src/Views/Welcome.elm
@@ -12,7 +12,13 @@ import Models exposing (..)
 view : Html Msg
 view =
     div [ class "welcome-page flex items-center justify-center" ]
-        [ h1 [ class "mt0" ]
-            [ text "mottoto"
+        [ div [ class "center" ]
+            [ h1 [ class "mt0" ]
+                [ text "mottoto"
+                ]
+            , div [ class "m0" ]
+                [ button [ class "button button-primary button-large mx1" ] [ text "login" ]
+                , button [ class "button button-primary button-large mx1" ] [ text "browse" ]
+                ]
             ]
         ]
