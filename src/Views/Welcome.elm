4a379bd7121c9795728dcea85d3af2a391d02921
Grant Schulte <schulte.grant@gmail.com>
2017-08-14 07:12:34 -0400

    Welcome page elements and styling

--- src/Views/Welcome.elm
+++ src/Views/Welcome.elm
@@ -12,13 +12,21 @@ import Models exposing (..)
 view : Html Msg
 view =
     div [ class "welcome-page flex items-center justify-center" ]
-        [ div [ class "center" ]
-            [ h1 [ class "mt0" ]
-                [ text "mottoto"
+        [ div [ class "max-width-2 center" ]
+            [ div [ class "mb3" ]
+                [ h1 [ class "mt0 mb2 h1" ] [ text "mottoto" ]
+                , div [ class "m2" ]
+                    [ span [ class "dot" ] []
+                    ]
+                , p [ class "mt0 mb1 h3 italic" ] [ text "noun" ]
+                , p [ class "m0 h3" ] [ text "a short sentence or phrase chosen as encapsulating the beliefs or ideals guiding an individual, family, or institution." ]
                 ]
             , div [ class "m0" ]
-                [ button [ class "button button-primary button-large mx1" ] [ text "login" ]
-                , button [ class "button button-primary button-large mx1" ] [ text "browse" ]
+                [ p [ class "h3" ]
+                    [ span [ class "underline" ] [ text "compose" ]
+                    , text " a motto "
+                    , span [ class "underline" ] [ text "or explore." ]
+                    ]
                 ]
             ]
         ]
