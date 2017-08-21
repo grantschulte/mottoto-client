10099271de2839120094f83aa16527f13769e410
Grant Schulte <schulte.grant@gmail.com>
2017-08-21 16:36:58 -0400

    Adds fields to profile edit

--- src/Views/EditProfile.elm
+++ src/Views/EditProfile.elm
@@ -12,15 +12,23 @@ view model =
         [ id "edit-profile-page"
         , class "flex items-center justify-center h100"
         ]
-        [ page ]
+        [ page model.authorizedUser ]
 
 
-page : Html Msg
-page =
+page : AuthorizedUser -> Html Msg
+page user =
     div [ class "col-12 md-col-4 center m1-auto" ]
         [ h1 [ class "mt0 mb2 h1 lh1" ] [ text "edit profile" ]
         , div [ class "form my3" ]
-            [ input [ type_ "text", placeholder "abelincoln" ] []
+            [ label [ class "block left-align h5 mb1" ]
+                [ text "handle" ]
+            , input [ type_ "text", class "mb2", placeholder user.handle ] []
+            , label [ class "block left-align h5 mb1" ]
+                [ text "email address" ]
+            , input [ type_ "email", class "mb2", placeholder user.email ] []
+            , label [ class "block left-align h5 mb1" ]
+                [ text "password" ]
+            , input [ type_ "password", class "mb2", placeholder "********" ] []
             ]
         , div []
             [ button [ class "button button-large button-primary w100" ]
